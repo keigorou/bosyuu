1c9b4d495ada47223b1f62f64213c1b82133dc88
keigorou <keigorou84@gmail.com>
2021-10-20 08:17:47 +0900

    Procfileの移動

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,7 @@ from django.urls.resolvers import URLPattern
 from . import views
 
 urlpatterns = [
+    path('/', views.ThanksView.as_view(), name='thanks'),
     path('thanks/', views.ThanksView.as_view(), name='thanks'),
     path('<slug:slug>/', views.IndexView.as_view(), name='index'),
     path('<slug:slug>/<int:pk>/', views.DetailView.as_view(), name='detail'),
